253a15e7db99620e26f0a8980039ee2d9439e88e
Daniel Antonio Ibarra Morantes - Ceiba Software <daniel.ibarra@ceiba.com.co>
2021-04-13 08:24:53 -0500

    añadimos las entidades iniciales para la logica negocio

---
+++ app/src/main/java/com/example/adn_danielibarra/mvp/modelo/entidad/Carro.java
@@ -0,0 +1,8 @@
+package com.example.adn_danielibarra.mvp.modelo.entidad;
+
+public class Carro extends Vehiculo {
+
+    public Carro (String placa) {
+        this.modificarPlaca(placa);
+    }
+}
---
+++ app/src/main/java/com/example/adn_danielibarra/mvp/modelo/entidad/Vehiculo.java
@@ -0,0 +1,35 @@
+package com.example.adn_danielibarra.mvp.modelo.entidad;
+
+import java.util.Calendar;
+
+public abstract class Vehiculo {
+    private String placa;
+    private Calendar fechaIngreso;
+    private Calendar fechaSalida;
+
+
+    public String obtenerPlaca() {
+        return placa;
+    }
+
+    public void modificarPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Calendar obtenerFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void modificarFechaIngreso(Calendar fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+
+    public Calendar obtenerFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void modificarFechaSalida(Calendar fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
