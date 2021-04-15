47fbeb43408624104b565978e7630623f3633d27
Daniel Antonio Ibarra Morantes - Ceiba Software <daniel.ibarra@ceiba.com.co>
2021-04-15 09:04:35 -0500

    Test del traductor

---
+++ infraestructura/src/test/java/com/example/infraestructura/anticorrupcion/CarroTraductorTest.java
@@ -0,0 +1,182 @@
+package com.example.infraestructura.anticorrupcion;
+
+import com.example.dominio.entidad.Carro;
+import com.example.infraestructura.entidadBd.CarroBd;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CarroTraductorTest {
+
+    @Test
+    public void mapearCarroDB_A_CarroDominioConObjetoNulo () {
+        CarroBd carroBd = null;
+        try {
+            CarroTraductor.mapearCarroBdACarroDominio(carroBd);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearCarroDB_A_CarroDominioConFechasNulas () {
+        CarroBd carroBd = new CarroBd();
+        try {
+            CarroTraductor.mapearCarroBdACarroDominio(carroBd);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearCarroDominio_A_CarroDBConObjetoNulo () {
+        Carro carro = null;
+        try {
+            CarroTraductor.mapearCarroDominioACarroDb(carro);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearCarroDominio_A_CarroDbConFechasNulas () {
+        Carro carro = new Carro("qwerty");
+        try {
+            CarroTraductor.mapearCarroDominioACarroDb(carro);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeCarroDbACarroDominioconListaNula () {
+        List<Carro> carroLista = null;
+
+        try {
+            CarroTraductor.mapearListaDeCarroDominioACarroDb(carroLista);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearListaDeCarroDbACarroDominioconListaConUnObjetoNulo () {
+        List<Carro> carroLista = carrosDefecto();
+        carroLista.add(null);
+
+        try {
+            CarroTraductor.mapearListaDeCarroDominioACarroDb(carroLista);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeCarroDbACarroDominioconListaConUnObjetoNuloVerificarSIOmiteEseObjeto () {
+        List<Carro> carroLista = carrosDefecto();
+        List<CarroBd> carroBdLista = new ArrayList<>();
+        carroLista.add(null);
+        carroLista.add(null);
+        carroLista.add(null);
+
+        try {
+            carroBdLista.addAll(CarroTraductor.mapearListaDeCarroDominioACarroDb(carroLista));
+        } catch (Exception e) {
+        }
+
+        assertEquals(carroLista.size()-3, carroBdLista.size());
+    }
+
+    @Test
+    public void mapearListaDeCarroDominioACarroDBconListaNula () {
+        List<CarroBd> carroDbLista = null;
+
+        try {
+            CarroTraductor.mapearListaDeCarroDbACarroDominio(carroDbLista);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearListaDeCarroDominioACarroDbconListaConUnObjetoNulo () {
+        List<Carro> carroLista = carrosDefecto();
+        List<CarroBd> carroBdLista = new ArrayList<>();
+        carroLista.add(null);
+
+        try {
+            CarroTraductor.mapearListaDeCarroDominioACarroDb(carroLista);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeCarroDbACarroDominioconListaConUnObjetoNuloVerificarSIOmiteEseObjetoz () {
+        List<Carro> carroLista = new ArrayList<>();
+        List<CarroBd> carroBdLista = carroBdDefecto();
+        carroBdLista.add(null);
+        carroBdLista.add(null);
+        carroBdLista.add(null);
+
+        try {
+            carroLista.addAll(CarroTraductor.mapearListaDeCarroDbACarroDominio(carroBdLista));
+        } catch (Exception e) {
+        }
+
+        assertEquals(carroBdLista.size()-3, carroLista.size());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private List<Carro> carrosDefecto () {
+        List<Carro> carroLista = new ArrayList<>();
+
+        Carro carro1 = new Carro("QWERTY");
+        Carro carro2 = new Carro("ZXCVB");
+        Carro carro3 = new Carro("UHJKNKL");
+        Carro carro4 = new Carro("SCVSVS");
+
+        carroLista.add(carro1);
+        carroLista.add(carro2);
+        carroLista.add(carro3);
+        carroLista.add(carro4);
+        return carroLista;
+    }
+
+    private List<CarroBd> carroBdDefecto () {
+        List<CarroBd> carroBdLista = new ArrayList<>();
+        CarroBd carroBd1 = new CarroBd();
+        CarroBd carroBd2 = new CarroBd();
+        CarroBd carroBd3 = new CarroBd();
+        CarroBd carroBd4 = new CarroBd();
+        CarroBd carroBd5 = new CarroBd();
+        carroBdLista.add(carroBd1);
+        carroBdLista.add(carroBd2);
+        carroBdLista.add(carroBd3);
+        carroBdLista.add(carroBd4);
+        carroBdLista.add(carroBd5);
+
+        return carroBdLista;
+    }
+}
---
+++ infraestructura/src/test/java/com/example/infraestructura/anticorrupcion/MotoTraductorTest.java
@@ -0,0 +1,182 @@
+package com.example.infraestructura.anticorrupcion;
+
+import com.example.dominio.entidad.Moto;
+import com.example.infraestructura.entidadBd.MotoBd;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class MotoTraductorTest {
+
+    @Test
+    public void mapearMotoDB_A_MotoDominioConObjetoNulo () {
+        MotoBd motoBd = null;
+        try {
+            MotoTraductor.mapearMotoBdAMotoDominio(motoBd);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearMotoDB_A_MotoDominioConFechasNulas () {
+        MotoBd motoBd = new MotoBd();
+        try {
+            MotoTraductor.mapearMotoBdAMotoDominio(motoBd);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearMotoDominio_A_MotoDBConObjetoNulo () {
+        Moto moto = null;
+        try {
+            MotoTraductor.mapearDeMotoDominioAMotoDb(moto);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearMotoDominio_A_MotoDbConFechasNulas () {
+        Moto moto = new Moto("qwerty",450);
+        try {
+            MotoTraductor.mapearDeMotoDominioAMotoDb(moto);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeMotoDbAMotoDominioconListaNula () {
+        List<Moto> motoLista = null;
+
+        try {
+            MotoTraductor.mapearListaDeMotoDominioAMotoDb(motoLista);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearListaDeMotoDbAMotoDominioconListaConUnObjetoNulo () {
+        List<Moto> motoLista = motosDefecto();
+        motoLista.add(null);
+
+        try {
+            MotoTraductor.mapearListaDeMotoDominioAMotoDb(motoLista);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeMotoDbAMotoDominioconListaConUnObjetoNuloVerificarSIOmiteEseObjeto () {
+        List<Moto> motoLista = motosDefecto();
+        List<MotoBd> motoBdLista = new ArrayList<>();
+        motoLista.add(null);
+        motoLista.add(null);
+        motoLista.add(null);
+
+        try {
+            motoBdLista.addAll(MotoTraductor.mapearListaDeMotoDominioAMotoDb(motoLista));
+        } catch (Exception e) {
+        }
+
+        assertEquals(motoLista.size()-3, motoBdLista.size());
+    }
+
+    @Test
+    public void mapearListaDeMotoDominioAMotoDBconListaNula () {
+        List<MotoBd> motoDbLista = null;
+
+        try {
+            MotoTraductor.mapearListaDeMotosDbAMotoDominio(motoDbLista);
+            fail();
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void mapearListaDeMotoDominioAMotoDbconListaConUnObjetoNulo () {
+        List<Moto> motoLista = motosDefecto();
+        List<MotoBd> motoBdLista = new ArrayList<>();
+        motoLista.add(null);
+
+        try {
+            MotoTraductor.mapearListaDeMotoDominioAMotoDb(motoLista);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void mapearListaDeMotoDbAMotoDominioconListaConUnObjetoNuloVerificarSIOmiteEseObjetoz () {
+        List<Moto> motoLista = new ArrayList<>();
+        List<MotoBd> motoBdLista = motoBdDefecto();
+        motoBdLista.add(null);
+        motoBdLista.add(null);
+        motoBdLista.add(null);
+
+        try {
+            motoLista.addAll(MotoTraductor.mapearListaDeMotosDbAMotoDominio(motoBdLista));
+        } catch (Exception e) {
+        }
+
+        assertEquals(motoBdLista.size()-3, motoLista.size());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private List<Moto> motosDefecto () {
+        List<Moto> motoLista = new ArrayList<>();
+
+        Moto moto1 = new Moto("QWERTY",400);
+        Moto moto2 = new Moto("ZXCVB",650);
+        Moto moto3 = new Moto("UHJKNKL",120);
+        Moto moto4 = new Moto("SCVSVS",850);
+
+        motoLista.add(moto1);
+        motoLista.add(moto2);
+        motoLista.add(moto3);
+        motoLista.add(moto4);
+        return motoLista;
+    }
+
+    private List<MotoBd> motoBdDefecto () {
+        List<MotoBd> motoBdLista = new ArrayList<>();
+        MotoBd motoBd1 = new MotoBd();
+        MotoBd motoBd2 = new MotoBd();
+        MotoBd motoBd3 = new MotoBd();
+        MotoBd motoBd4 = new MotoBd();
+        MotoBd motoBd5 = new MotoBd();
+        motoBdLista.add(motoBd1);
+        motoBdLista.add(motoBd2);
+        motoBdLista.add(motoBd3);
+        motoBdLista.add(motoBd4);
+        motoBdLista.add(motoBd5);
+
+        return motoBdLista;
+    }
+}
