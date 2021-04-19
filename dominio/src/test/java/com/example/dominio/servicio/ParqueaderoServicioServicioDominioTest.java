d748ba1dfb7d48f83069ef19737815572198b190
Daniel Antonio Ibarra Morantes - Ceiba Software <daniel.ibarra@ceiba.com.co>
2021-04-19 00:18:30 -0500

    segunda prueba reduccion cobertura

--- dominio/src/test/java/com/example/dominio/servicio/ParqueaderoServicioServicioDominioTest.java
+++ dominio/src/test/java/com/example/dominio/servicio/ParqueaderoServicioServicioDominioTest.java
@@ -1,7 +1,6 @@
 package com.example.dominio.servicio;
 
 import com.example.dominio.modelo.entidad.Carro;
-import com.example.dominio.modelo.entidad.Moto;
 import com.example.dominio.modelo.entidad.Vehiculo;
 import com.example.dominio.repositorio.VehiculoRepositorio;
 import com.example.dominio.servicio.contrato.ContratoParqueadero;
@@ -10,8 +9,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
 public class ParqueaderoServicioServicioDominioTest {
 
@@ -40,103 +39,7 @@ public class ParqueaderoServicioServicioDominioTest {
         //Assert
     }
 
-    @Test
-    public void guardarCarroTeniedoVeinteCarrosParqueados () {
-        //Arrange
-        Vehiculo vehiculo = new Carro("QWET-132");
-        when(carroRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(20);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarCarro(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarMotoTeniedoCincoMotosParqueados () {
-        //Arrange
-        Vehiculo vehiculo = new Moto("QWET-132", 200);
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(5);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-        } catch (Exception e) {
-            fail();
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarMotoTeniedoDiezMotoParqueados () {
-        //Arrange
-        Vehiculo vehiculo = new Moto("QWET-132", 200);
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(10);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarCarroConPlacaNula () {
-        //Arrange
-        Vehiculo vehiculo = new Carro(null);
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(2);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarMotoConPlacaNula () {
-        //Arrange
-        Vehiculo vehiculo = new Moto(null, 200);
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(2);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarCarroConPlacaVacia () {
-        //Arrange
-        Vehiculo vehiculo = new Carro("");
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(2);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
-
-    @Test
-    public void guardarMotoConPlacaVacia () {
-        //Arrange
-        Vehiculo vehiculo = new Moto("", 200);
-        when(motoRepositorioPersistencia.obtenerCantidadVehiculos()).thenReturn(2);
-        //Act
-        try {
-            parqueaderoServicioDominio.guardarMoto(vehiculo);
-            fail();
-        } catch (Exception e) {
-        }
-        //Assert
-    }
+   
 
 
 }
